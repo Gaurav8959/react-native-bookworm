7ddde15f59e6add0283ba5138171df29e9d578e3
Gourav8959 <gourav.rathore@aripratech.com>
2025-04-16 18:36:47 +0530

    cron job

--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ import "dotenv/config"
 import router from "./routes/authRoutes.js";
 import bookroutes from "./routes/bookRoutes.js"
 import { connectDB } from "./lib/db.js";
+import job from "./lib/cron.js";
 
 const app = express();
 app.use(express.json());
 app.use(cors())
 const PORT = process.env.PORT || 3000;
 
+job.start();
 app.use("/api/auth", router);
 app.use("api/books", bookroutes);
 
