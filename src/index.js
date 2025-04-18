3e855d8419009a3df30eb70dd95ab032ccca9f77
Gourav8959 <gourav.rathore@aripratech.com>
2025-04-18 16:05:15 +0530

    Chnages

--- src/index.js
+++ src/index.js
@@ -8,10 +8,9 @@ import job from "./lib/cron.js";
 
 const app = express();
 
-
+// app.use(express.json());
 app.use(cors())
-app.use(express.json());
-// app.use(express.json({ limit: '50mb' }));
+app.use(express.json({ limit: '50mb' }));
 const PORT = process.env.PORT || 3000;
 
 job.start();
