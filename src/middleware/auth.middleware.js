2bf7588f1ca514f4821018c2bd6436dcbd46c634
Gourav8959 <gourav.rathore@aripratech.com>
2025-04-18 14:05:54 +0530

    Hello

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -7,12 +7,12 @@ const protectRoute = async (req, res, next) => {
         if(!token) return res.status(401).json({ message: "No  autherization token, access denied"});
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
         const user = await User.findById(decoded.userId).select("-password");
-        if(!user) return res.status(401).json({ message: "Token is not valid"});
+        if(!user) return res.status(401).json({ message: "Token is not valid!"});
         req.user = user;
         next();
     } catch (error) {
         console.log("Authentication error",error.message);
-        res.status(401).json({ message: "Token is not valid"});
+        res.status(401).json({ message: "Token is not valid?"});
     }
 };
 
