37011af0f40d50a2de9cc9b4c9ca5a3ea6f7f972
Gourav8959 <gourav.rathore@aripratech.com>
2025-04-18 13:28:52 +0530

    Changes

--- src/index.js
+++ src/index.js
@@ -7,13 +7,15 @@ import { connectDB } from "./lib/db.js";
 import job from "./lib/cron.js";
 
 const app = express();
-app.use(express.json());
+
+// app.use(express.json());
 app.use(cors())
+app.use(express.json({ limit: '50mb' }));
 const PORT = process.env.PORT || 3000;
 
 job.start();
 app.use("/api/auth", router);
-app.use("api/books", bookroutes);
+app.use("/api/books", bookroutes);
 
 app.listen(PORT, () => {
     console.log(`Server is running on PORT No: ${PORT}`);
