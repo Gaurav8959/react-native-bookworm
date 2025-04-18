be7f7738d0217401ba69b554719b7814bd679503
Gourav8959 <gourav.rathore@aripratech.com>
2025-04-18 14:11:09 +0530

    Chnages

--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -5,7 +5,7 @@ import protectRoute from "../middleware/auth.middleware.js";
 
 const bookroutes = express.Router();
 //Create Book
-bookroutes.post("/", protectRoute,async (req, res) => {
+bookroutes.post("/",async (req, res) => {
     try {
         const { title, caption, rating, image} = req.body;
 
